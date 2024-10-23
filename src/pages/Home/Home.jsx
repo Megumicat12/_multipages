d9631f598492fd417d8734b3c24a7c97a20e6519
Megumicat21 <yanisa.int@spulive.net>
2024-10-23 22:14:29 +0700

    Init

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,56 @@
+import './Home.css'
+function Home() {
+    return (
+        <div className='font'>
+            <div className='home-container'>
+                <h1>INTRODUCE MYSELF </h1>
+                <div className='introduce-container'>
+                  
+                    <div className='img-container'>
+                        <iframe src="https://giphy.com/embed/1fhj2RprUOpqCObj2J" 
+                                width="150" 
+                                height="150" 
+                                frameBorder="0" 
+                                className="giphy-embed" 
+                                allowFullScreen>
+                        </iframe>
+                    </div>
+                    
+                    <div className='text-container'>
+                    
+                        <h2><u><iframe src="https://giphy.com/embed/uDnoWfNk42Co8SMqo6" 
+                                width="100" 
+                                height="50" 
+                                frameBorder="0" 
+                                className="tenor-embed" 
+                                allowFullScreen>
+                        </iframe>CREAM<iframe src="https://giphy.com/embed/uDnoWfNk42Co8SMqo6" 
+                                width="100" 
+                                height="50" 
+                                frameBorder="0" 
+                                className="tenor-embed" 
+                                allowFullScreen>
+                        </iframe></u></h2> 
+                        
+                        <p className='text'>
+                        I am CREAM, and my full name is Yanisa Intharawicha. I'm 19 years old and currently a student at SPU. I have a number of interests that reflect my personality clearly. My favorite food is ramen, which showcases not only my love for Japanese cuisine but also my appreciation for simplicity and the joy found in small details. Additionally, my favorite color is white, signifying my love for purity, simplicity, and calmness.       
+                        </p>
+                        
+                        <iframe src="https://giphy.com/embed/EGr7Lxw3A37CVvxnoo" 
+                                width="150" 
+                                height="150" 
+                                frameBorder="0" 
+                                className="tenor-embed" 
+                                allowFullScreen>
+                                    
+                        </iframe>
+                    </div>
+                    
+                </div>
+               
+            </div>
+        </div>
+    )
+}
+
+export default Home;
